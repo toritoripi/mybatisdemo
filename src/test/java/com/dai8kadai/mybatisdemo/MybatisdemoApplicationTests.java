bdf85eddc076026f2af81273ca30aedc88251dfb
Your Name <[chicken26862@icloud.com]>
2023-01-14 16:28:58 +0900

    プロジェクトのセットアップ

---
+++ src/test/java/com/dai8kadai/mybatisdemo/MybatisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dai8kadai.mybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
